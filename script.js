61801f16677b569288e6230aa174d6dbcfc2d04b
JDean2814 <jdean2814@gmail.com>
2022-11-16 15:21:55 -0600

    setup winner display msg

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const gameMess = document.querySelector('.game_message');
+const gameResult = document.querySelector('#game_result');
+const restartBtn = document.getElementById('restart_game_btn');
 const boardSpaces = document.querySelectorAll('.board_spaces');
 let boardArr = ['','','','','','','','',''];
 const WINNER_ARRAY = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
@@ -19,10 +22,10 @@ function handleClick(e) {
         for (let j = 0; j < WINNER_ARRAY[i].length; j++) {
             arr.push(boardArr[WINNER_ARRAY[i][j]]);
         };
-        console.log(arr);
 
         if (arr.every((element) => element === character)) {
-            console.log(`${character} is the winner!`);
+            gameMess.style.display = "flex";
+            gameResult.innerHTML = `'${character.toUpperCase()}' wins!`
         };
         arr = [];
     };
@@ -31,4 +34,11 @@ function handleClick(e) {
 
 boardSpaces.forEach(element => {
     element.addEventListener('click', (e) => handleClick(e));
+});
+
+//Restart game button
+restartBtn.addEventListener('click', () => {
+    boardArr = ['','','','','','','','',''];
+    boardSpaces.forEach((element) => element.innerHTML = '');
+    gameMess.style.display = "none";
 });
