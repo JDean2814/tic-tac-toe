de903ad40572eecd479d4f1eef8b14f1bd7290c0
JDean2814 <jdean2814@gmail.com>
2022-11-16 14:45:46 -0600

    Got the game function working

---
+++ script.js
@@ -0,0 +1,34 @@
+const boardSpaces = document.querySelectorAll('.board_spaces');
+let boardArr = ['','','','','','','','',''];
+const WINNER_ARRAY = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let character = 'x';
+
+function charSwap() {
+    character === 'x' ? character = 'o' : character = 'x';
+    return character;
+};
+
+function handleClick(e) {
+    let arr = [];
+    e.target.innerHTML = character;
+    for (let i = 0; i < boardSpaces.length; i++) {
+        boardArr[i] = boardSpaces[i].innerHTML;
+    };
+
+    for (let i = 0; i < WINNER_ARRAY.length; i++) {
+        for (let j = 0; j < WINNER_ARRAY[i].length; j++) {
+            arr.push(boardArr[WINNER_ARRAY[i][j]]);
+        };
+        console.log(arr);
+
+        if (arr.every((element) => element === character)) {
+            console.log(`${character} is the winner!`);
+        };
+        arr = [];
+    };
+    character = charSwap();
+};
+
+boardSpaces.forEach(element => {
+    element.addEventListener('click', (e) => handleClick(e));
+});
