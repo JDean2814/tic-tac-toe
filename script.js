0a8655ccad486aaf7c4331edd2db9e7f783f62d7
JDean2814 <jdean2814@gmail.com>
2022-11-17 12:56:02 -0600

    Added hover effects

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const gameMess = document.querySelector('.game_message');
 const gameResult = document.querySelector('#game_result');
 const restartBtn = document.getElementById('restart_game_btn');
 const boardSpaces = document.querySelectorAll('.board_spaces');
+const charBtns = document.querySelectorAll('.char_btn');
+
 let boardArr = ['','','','','','','','',''];
 const WINNER_ARRAY = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let character = 'x';
+let charSelectTrue = false;
+
+
+charBtns.forEach(element => {
+    element.addEventListener('click', (e) => {
+        if (!charSelectTrue) {
+            character = e.target.innerHTML;
+            charSelectTrue = true;
+        } else {
+            return;
+        }
+        
+    });
+});
+
 
 function charSwap() {
     character === 'x' ? character = 'o' : character = 'x';
@@ -26,6 +43,10 @@ function handleClick(e) {
         if (arr.every((element) => element === character)) {
             gameMess.style.display = "flex";
             gameResult.innerHTML = `'${character.toUpperCase()}' wins!`
+            return;
+        } else if(boardArr.every((element) => element != '')) {
+            gameMess.style.display = "flex";
+            gameResult.innerHTML = `It is a draw!`;
         };
         arr = [];
     };
